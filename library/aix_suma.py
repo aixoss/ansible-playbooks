fa10d4eec2c70e9ababa1487ba66d551ab2d645a
cyril-atos <c.bouhallier@atos.net>
2017-02-10 15:27:09 +0100

    Updated

--- library/aix_suma.py
+++ library/aix_suma.py
@@ -89,12 +89,12 @@ def main():
     # Get Module params
     # ===========================================
 
+    # TODO (CGB): Update
+
     if module.params['targets']:
         targets = module.params['targets']
     else:
         raise Exception("Error: Targets field is empty")
-
-    # TODO (CGB): Update
 
     debug_data = []
 
@@ -119,11 +119,6 @@ def main():
     for line in std_out.rstrip().split('\n'):
         nim_clients.append(line.split()[0])
 
-    #########################################################
-    # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG #
-    #########################################################
-    debug_data.append('NIM Clients: {}'.format(nim_clients))
-
     # ==========================================================================
     # Launch threads
     # ==========================================================================
@@ -139,11 +134,6 @@ def main():
     for process in threads:
         process.join()
 
-    #########################################################
-    # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG #
-    #########################################################
-    debug_data.append('oslevel Dict: {}'.format(oslevel_list))
-
     # ==========================================================================
     # Build targets list
     # ==========================================================================
@@ -151,48 +141,39 @@ def main():
     target_results = []
     targets_list = targets.split(' ')
 
-    #########################################################
-    # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG #
-    #########################################################
-    debug_data.append('Targets List: {}'.format(targets_list))
-
     for target in targets_list:
 
+        #------------------------------------------------------------
         # Build target(s) from: range i.e. quimby[7:12]
-        #---------------------------------------------
+        #------------------------------------------------------------
 
-        m = re.search('\[[0-9]+:[0-9]+\]', target)
+        m = re.search('(\w+)\[(\d+):(\d+)\]', target)
         if m:
-            name = target.split('[')[0]
-            # List of Strings
-            target_range = re.findall('[0-9]+', target)
-            # Convert List of string to List of Integer
-            target_range = [int(i) for i in target_range]
-            for i in range(target_range[0], target_range[1] + 1):
-                # Display number with leading zeros
-                target_results.append(name + str(format(i, '02d')))
 
+            name, indices = targets[:-1].split('[')
+            start, end = indices.split(':')
+
+            for i in range(int(start), int(end)+1):
+                target_results.append('{0}{1:02}'.format(name, i))
+
+        #------------------------------------------------------------
         # Build target(s) from: all
-        #-----------------------------------------------
+        #------------------------------------------------------------
 
         m = re.search('[Aa][Ll][Ll]', target)
         if m:
             target_results = nim_clients
 
+        #------------------------------------------------------------
         # Build target(s) from: *
-        #-----------------------------------------------
+        #------------------------------------------------------------
 
         m = re.search('[*]', target)
         if m:
             target_results = nim_clients
 
-    # # Join two lists WITHOUT duplicate
+    # Join two lists WITHOUT duplicate
     clients = list(set(target_results) & set(nim_clients))
-
-    #########################################################
-    # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG #
-    #########################################################
-    debug_data.append('Targets Range: {}'.format(clients))
 
     # ==========================================================================
     # Generate oslevel dictionnary from 'nim clients' on target machines
@@ -205,35 +186,20 @@ def main():
             targets_oslevel[client] = oslevel_list[client]
 
     # Min oslevel from the targets_oslevel
-    min_oslvl=min_oslevel(targets_oslevel)[0:7]
-
-    #########################################################
-    # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG #
-    #########################################################
-    debug_data.append('{} <= Min Oslevel'.format(min_oslvl))
+    min_oslvl = min_oslevel(targets_oslevel)[:7]
 
     # ==========================================================================
     # Build suma command
     # ==========================================================================
 
-    # suma -x -a Action=Download
-    #         -a DisplayName="Download SP"
-    #         -a RqType=SP
-    #         -a FilterML=7100-02
-    #         -a RqName=7100-02-02-1316
-
-    suma_action   = '-a Action=Download '
-    suma_display  = '-a DisplayName="Download SP"'
-    suma_rqtype   = '-a RqType=SP '
+    suma_task = '/usr/sbin/suma -x '
+    suma_action = '-a Action=Download '
+    suma_display = '-a DisplayName="Download SP" '
+    suma_rqtype = '-a RqType=SP '
     suma_filterml = '-a FilterML={} '.format(min_oslvl)
-    suma_rqname   = '-a RqName={} '.format(min_oslevel(targets_oslevel))
+    suma_rqname = '-a RqName={} '.format(min_oslevel(targets_oslevel))
 
-    suma_cmd = "/usr/sbin/suma -x " + suma_action + suma_rqtype + suma_filterml + suma_rqname
-
-    #########################################################
-    # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG # DEBUG #
-    #########################################################
-    debug_data.append('suma command:{}'.format(suma_cmd))
+    suma_cmd = ''.join((suma_task, suma_action, suma_rqtype, suma_filterml, suma_rqname, suma_display))
 
 
     rc, stdout, stderr = module.run_command(suma_cmd)
@@ -248,14 +214,6 @@ def main():
             module.fail_json(msg="SUMA Execute: {} => Error :{}".format(suma_cmd, stderr.split('\n')))
         else:
             module.fail_json(msg="SUMA Execute: {} => Output :{}".format(suma_cmd, stdout.split('\n')))
-
-###########################################################################################################
-
-    # module.exit_json(
-    #     changed=False,
-    #     msg="Command: {} => Data: {}".format(cmd, nim_clients),
-    #     debug_targets=debug_data)
-    #     # debug_targets=clients,
 
 ###########################################################################################################
 
